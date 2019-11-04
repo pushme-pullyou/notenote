d7bf2dad9f3f7a22823389749ec6598eef93f9dd
Theo Armour <theo-armour@users.noreply.github.com>
2019-11-03 17:59:22 -0800

    second commit

--- v-0-00/jsonbox-0-00.js
+++ v-0-00/jsonbox-0-00.js
@@ -1,27 +1,43 @@
+// copyright pushMe-pullYou authors. MIT license.
+// https://jsonbox.io/
+
 "use strict";
 
 const JB = {};
 
-JB.url = "https://jsonbox.io/box_34acd03fcbcfd2fa1051";
 
-window.addEventListener("focus", () => JB.getJson());
 
-JB.getJson = function() {
-	if (!JB.interval) {
-		JB.interval = setInterval(JB.putJson, 2000);
-	}
+JB.getJson = function () {
 
-	window.onunload = JB.putJson("");
+	JB.urlBase = "https://jsonbox.io/"
+	JB.boxId = localStorage.getItem("boxId") || ""; // "box_34acd03fcbcfd2fa1051";
+	JB.recordId = localStorage.getItem("recordId") || "/5db548fe8d6b3900173a5721";
 
-	const request = new Request(JB.url + "/5db548fe8d6b3900173a5721");
+	if (!JB.boxId) { return; }
+
+	window.addEventListener("focus", () => JB.getJson());
+
+	inpBoxId.value = JB.boxId;
+	console.log('boxId', JB.boxId );
+
+	inpRecordId.value = JB.recordId;
+	console.log('recordId', JB.recordId);
+
+	JB.url = JB.urlBase + JB.boxId + JB.recordId;
+	console.log('JB.url', JB.url);
+
+	const request = new Request( JB.url );
 	fetch(request)
-		.then(response => response.json())
-		.then(data => {
-			JB.json = data;
-			JB.contentPrevious = divJsonBox.innerHTML = JB.json.content;
-			console.log("JB.json", JB.json);
-		});
+	.then(response => response.json())
+	.then(data => {
+		JB.json = data;
+		JB.contentPrevious = divJsonBox.innerHTML = JB.json.content;
+		JB.interval = setInterval(JB.putJson, 2000);
+		console.log("JB.json", JB.json);
+		window.onunload = JB.putJson("");
+	});
 };
+
 
 JB.putJson = function(previous = JB.contentPrevious) {
 	const content = divJsonBox.innerHTML;
@@ -32,16 +48,35 @@ JB.putJson = function(previous = JB.contentPrevious) {
 
 	JB.contentPrevious = content;
 
-	fetch(JB.url + "/5db548fe8d6b3900173a5721", {
+	fetch( JB.url, {
 		method: "PUT",
 		headers: { "content-type": "application/json" },
 		body: JSON.stringify({ content: content })
 	})
-		.then(response => response.json())
-		.then(data => {
-			divJsonBoxResponse.innerHTML = `<p>${new Date().toLocaleTimeString()}</p>${content}`;
-		});
+	.then(response => response.json())
+	.then(data => {
+		//console.log('data', data);
+		divJsonBoxResponse.innerHTML = `${new Date().toLocaleTimeString()}: ${data.message}`;
+	});
+	
 };
+
+
+
+JB.setLocalStorage = function() {
+
+	JB.boxId = inpBoxId.value || "";
+
+	localStorage.setItem("boxId", JB.boxId );
+
+	JB.RecordId = "/" + ( inpRecordId.value || "5db548fe8d6b3900173a5721" );
+
+	localStorage.setItem("recordId", JB.recordId);
+
+	JB.getJson();
+
+}
+
 
 JB.getJsonAll = function() {
 	const request = new Request(JB.url);
